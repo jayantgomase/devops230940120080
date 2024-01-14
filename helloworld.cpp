e9d1c000dc192c324bda1fed0f205357e5cc55dd
jayantgomase <jayantgomase3@gmail.com>
2024-01-14 15:45:02 +0530

    helloworld cpp

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+
+#include <iostream>
+
+int main()
+{
+    string name;
+    cout << "What is your name?: ";
+    getline(cin, name);
+    cout << "Hello " << name << endl;
+    return 0;
+}
